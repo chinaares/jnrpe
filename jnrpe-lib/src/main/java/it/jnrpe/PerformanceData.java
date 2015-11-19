b60a75b9d51295ac05d68fa7bfbf3eff7a5bc3cb
Ares <yangzibin.cn@hotmail.com>
2015-11-19 13:36:39 +0800

    添加performanceData的nagios规范注释

--- jnrpe-lib/src/main/java/it/jnrpe/PerformanceData.java
+++ jnrpe-lib/src/main/java/it/jnrpe/PerformanceData.java
@@ -26,6 +26,62 @@ import it.jnrpe.utils.thresholds.Prefixes;
  *
  * Refer to http://nagiosplug.sourceforge.net/developer-guidelines.html#AEN201
  *
+ * Nagios 3 and newer will concatenate the parts following a "|" in a) the first
+ * line output by the plugin, and b) in the second to last line, into a string
+ * it passes to whatever performance data processing it has configured. (Note
+ * that it currently does not insert additional whitespace between both, so the
+ * plugin needs to provide some to prevent the last pair of a) and the first of
+ * b) getting run together.) Please refer to the Nagios documentation for
+ * information on how to configure such processing. However, it is the
+ * responsibility of the plugin writer to ensure the performance data is in a
+ * "Nagios Plugins" format. This is the expected format:
+ * 
+ * 'label'=value[UOM];[warn];[crit];[min];[max]
+ * 
+ * Notes:
+ * 
+ * space separated list of label/value pairs
+ * 
+ * label can contain any characters except the equals sign or single quote (')
+ * 
+ * the single quotes for the label are optional. Required if spaces are in the
+ * label
+ * 
+ * label length is arbitrary, but ideally the first 19 characters are unique
+ * (due to a limitation in RRD). Be aware of a limitation in the amount of data
+ * that NRPE returns to Nagios
+ * 
+ * to specify a quote character, use two single quotes
+ * 
+ * warn, crit, min or max may be null (for example, if the threshold is not
+ * defined or min and max do not apply). Trailing unfilled semicolons can be
+ * dropped
+ * 
+ * min and max are not required if UOM=%
+ * 
+ * value, min and max in class [-0-9.]. Must all be the same UOM. value may be a
+ * literal "U" instead, this would indicate that the actual value couldn't be
+ * determined
+ * 
+ * warn and crit are in the range format (see the Section called Threshold and
+ * ranges). Must be the same UOM
+ * 
+ * UOM (unit of measurement) is one of:
+ * 
+ * no unit specified - assume a number (int or float) of things (eg, users,
+ * processes, load averages)
+ * 
+ * s - seconds (also us, ms)
+ * 
+ * % - percentage
+ * 
+ * B - bytes (also KB, MB, TB)
+ * 
+ * c - a continous counter (such as bytes transmitted on an interface)
+ * 
+ * It is up to third party programs to convert the Nagios Plugins performance
+ * data into graphs.
+ *
  * @author Massimiliano Ziccardi
  * @version $Revision: 1.0 $
  */
